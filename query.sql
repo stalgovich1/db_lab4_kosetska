241fc6a3d7b21795d2f90f4ad76e749cca1132a2
stalgovich1 <91222399+stalgovich1@users.noreply.github.com>
2023-12-26 19:52:42 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,17 +1,11 @@
--- Загальний рейтинг (weekly_rank) для фільму "Red Notice" за всі країни
-SELECT ac.show_title, ac.country_name, ag.weekly_rank FROM  all_weeks_countries ac
-JOIN all_weeks_global ag ON ac.show_title = ag.show_title
-WHERE ac.show_title = 'Red Notice'
-UNION ALL
+--Вибрати всі фільми
+SELECT * FROM MoviesAndShows;
 
--- Кількість годин перегляду для кожного фільму в Аргентині
-SELECT ac.show_title, ac.country_name, mp.hours_viewed_first_28_days
-FROM  all_weeks_countries ac
-JOIN most_popular mp ON ac.show_title = mp.show_title
-WHERE ac.country_name = 'Argentina'
-UNION ALL
+--Вибрати всі країни
+SELECT * FROM Countries;
 
--- Загальний рейтинг (weekly_rank) для фільму за всі країни
-SELECT ac.show_title, ac.country_name
-FROM all_weeks_countries ac
-JOIN all_weeks_global ag ON ac.show_title = ag.show_title;
+--Вибрати всі рейтинги разом із назвою фільму/шоу та ім'ям країни
+SELECT MoviesAndShows.Title, Countries.CountryName, Ratings.Rank, Ratings.Viewership, Ratings.Duration
+FROM Ratings
+JOIN MoviesAndShows ON Ratings.MovieOrShowID = MoviesAndShows.MovieOrShowID
+JOIN Countries ON Ratings.CountryID = Countries.CountryID;
