27291d5baef149de6f63faed3711e565f1cec052
stalgovich1 <91222399+stalgovich1@users.noreply.github.com>
2023-12-26 13:11:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import psycopg2
 import matplotlib.pyplot as plt
 
 username = 'postgres'
-password = '5432'
+password = '111'
 database = 'db_lab3'
 host = 'localhost'
 port = '5432'
@@ -12,28 +12,26 @@ query_1 = '''
 SELECT ac.show_title, ac.country_name, ag.weekly_rank FROM  all_weeks_countries ac
 JOIN all_weeks_global ag ON ac.show_title = ag.show_title
 WHERE ac.show_title = 'Red Notice'
-UNION ALL
-
 '''
+
 query_2 = '''
 -- Кількість годин перегляду для кожного фільму в Аргентині
 SELECT ac.show_title, ac.country_name, mp.hours_viewed_first_28_days
 FROM  all_weeks_countries ac
 JOIN most_popular mp ON ac.show_title = mp.show_title
 WHERE ac.country_name = 'Argentina'
-UNION ALL
 '''
+
 query_3 = '''
 -- Загальний рейтинг (weekly_rank) для фільму за всі країни
 SELECT ac.show_title, ac.country_name
 FROM all_weeks_countries ac
-JOIN all_weeks_global ag ON ac.show_title = ag.show_title;
+JOIN all_weeks_global ag ON ac.show_title = ag.show_title
 '''
 
 conn = psycopg2.connect(user=username, password=password, dbname=database, host=host, port=port)
 
 with conn:
-                       
     cur = conn.cursor()
 
     print('Загальний рейтинг (weekly_rank) для фільму "Red Notice" за всі країни:')
