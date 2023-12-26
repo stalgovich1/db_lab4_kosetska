f178242e847f48bdd64a99560210e29a2a92ca0b
stalgovich1 <91222399+stalgovich1@users.noreply.github.com>
2023-12-26 11:48:06 +0200

    Create populate.sql

---
+++ populate.sql
@@ -0,0 +1,58 @@
+-- Вставка даних в таблицю MoviesAndShows
+INSERT INTO MoviesAndShows (MovieOrShowID, Title, Type, ReleaseYear)
+VALUES
+    (1, 'Look Both Ways', 'Films', NULL),
+    (2, 'Day Shift', 'Films', NULL),
+    (3, 'Bank Robbers: The Last Great Heist', 'Films', NULL),
+    (4, 'The Next 365 Days', 'Films', NULL),
+    (5, 'The Angry Birds Movie 2', 'Films', NULL),
+    (6, 'Purple Hearts', 'Films', NULL),
+    (7, 'River Runs Red', 'Films', NULL),
+    (8, '1917', 'Films', NULL),
+    (9, 'Code Name: Emperor', 'Films', NULL);
+
+-- Вставка даних в таблицю Weeks
+INSERT INTO Weeks (WeekID, WeekStartDate, WeekEndDate)
+VALUES
+    (1, '2022-08-21', '2022-08-21');
+
+-- Вставка даних в таблицю Countries
+INSERT INTO Countries (CountryID, CountryName)
+VALUES
+    (1, 'Argentina');
+
+-- Вставка даних в таблицю Ratings
+INSERT INTO Ratings (RatingID, MovieOrShowID, WeekID, CountryID, Rank, Viewership, Duration)
+VALUES
+    (1, 1, 1, 1, 1, NULL, NULL),
+    (2, 2, 1, 1, 2, NULL, NULL),
+    (3, 3, 1, 1, 2, NULL, NULL),
+    (4, 4, 1, 1, 1, NULL, NULL),
+    (5, 5, 1, 1, 1, NULL, NULL),
+    (6, 6, 1, 1, 4, NULL, NULL),
+    (7, 7, 1, 1, 1, NULL, NULL),
+    (8, 8, 1, 1, 2, NULL, NULL),
+    (9, 9, 1, 1, 2, NULL, NULL);
+
+-- Вставка даних в таблицю Ratings (глобальні)
+INSERT INTO Ratings (RatingID, MovieOrShowID, WeekID, CountryID, Rank, Viewership, Duration)
+VALUES
+    (10, 1, 1, NULL, 1, 63390000, 2),
+    (11, 2, 1, NULL, 2, 48060000, 1),
+    (12, 4, 1, NULL, 1, 39310000, 1),
+    (13, 6, 1, NULL, 4, 23410000, 4),
+    (14, 5, 1, NULL, 1, 20050000, 1),
+    (15, 9, 1, NULL, 2, 11470000, 1),
+    (16, 8, 1, NULL, 3, 8660000, 3);
+
+-- Вставка даних в таблицю most_popular
+INSERT INTO most_popular (Type, Rank, Title, Duration, Viewership)
+VALUES
+    ('Films (English)', 1, 'Red Notice', NULL, 364020000),
+    ('Films (English)', 2, 'Don''t Look Up', NULL, 359790000),
+    ('Films (English)', 3, 'Bird Box', NULL, 282020000),
+    ('Films (English)', 4, 'The Gray Man', NULL, 253830000),
+    ('Films (English)', 5, 'The Adam Project', NULL, 233160000),
+    ('Films (English)', 6, 'Extraction', NULL, 231340000),
+    ('Films (English)', 7, 'Purple Hearts', NULL, 220610000),
+    ('Films (English)', 8, 'The Unforgivable', NULL, 214700000);
