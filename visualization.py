fc08d4e664f729dd6f84f282bc21492f88ac3623
stalgovich1 <91222399+stalgovich1@users.noreply.github.com>
2023-12-26 22:59:42 +0200

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -2,69 +2,81 @@ import psycopg2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Параметри підключення до бази даних
-connection_params = {
-    'user': 'postgres',
-    'password': 'postgres',
-    'dbname': 'lab_3_6',
-    'host': 'localhost',
-    'port': '5432'
-}
 
-# SQL-запити
+username = 'postgres'
+password = 'postgres'
+database = 'lab_3_6'
+host = 'localhost'
+port = '5432'
+
 query_1 = '''
-SELECT ReleaseYear, AVG(Duration) AS AvgDuration
-FROM Ratings
-JOIN MoviesAndShows ON Ratings.MovieOrShowID = MoviesAndShows.MovieOrShowID
-GROUP BY ReleaseYear
-ORDER BY ReleaseYear;
+SELECT R.Rank, AVG(R.Duration) AS AverageDuration
+FROM Ratings R
+GROUP BY R.Rank;
 '''
-
 query_2 = '''
-SELECT CountryName, SUM(Viewership) AS TotalViewership
-FROM Ratings
-JOIN Countries ON Ratings.CountryID = Countries.CountryID
-GROUP BY CountryName
-ORDER BY TotalViewership DESC;
+SELECT R.CountryID, COUNT(*) AS Count
+FROM Ratings R
+GROUP BY R.CountryID
+ORDER BY Count DESC
+LIMIT 5;
 '''
-
 query_3 = '''
-SELECT Type, AVG(Duration) AS AvgDuration
-FROM Ratings
-JOIN MoviesAndShows ON Ratings.MovieOrShowID = MoviesAndShows.MovieOrShowID
-GROUP BY Type;
+SELECT M.ReleaseYear, AVG(R.Rank) AS AverageRank
+FROM MoviesAndShows M
+JOIN Ratings R ON M.MovieOrShowID = R.MovieOrShowID
+GROUP BY M.ReleaseYear
+ORDER BY M.ReleaseYear;
 '''
 
-# Встановлення з'єднання з базою даних
-with psycopg2.connect(**connection_params) as conn:
-    with conn.cursor() as cur:
-        # Виконання запитів та отримання результатів
-        cur.execute(query_1)
-        years_1, durations_1 = zip(*cur.fetchall())
+conn = psycopg2.connect(user=username, password=password, dbname=database, host=host, port=port)
 
-        cur.execute(query_2)
-        countries, total_viewership = zip(*cur.fetchall())
+with conn:
+    cur = conn.cursor()
 
-        cur.execute(query_3)
-        types, avg_durations = zip(*cur.fetchall())
+    # Task 1
+    cur.execute(query_1)
+    ranks = []
+    durations = []
 
-# Побудова візуалізацій
-fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+    for row in cur:
+        ranks.append(row[0])
+        durations.append(row[1])
 
-# Task 1
-ax[0].bar(years_1, durations_1, color='g', edgecolor='y')
-ax[0].set_title('Середня тривалість фільмів/шоу за роками')
-ax[0].set_xlabel('Рік')
-ax[0].set_ylabel('Середня тривалість')
+    fig, ax = plt.subplots(1, 3)
+    ax[0].bar(ranks, durations, color='g', edgecolor='y')
+    ax[0].set_title('Average Duration by Rank')
+    ax[0].set_xlabel('Rank')
+    ax[0].set_ylabel('Average Duration')
 
-# Task 2
-ax[1].pie(total_viewership, labels=countries, autopct='%1.1f%%', wedgeprops={'edgecolor': 'y'})
-ax[1].set_title('Загальна кількість переглядів за країною')
+    # Task 2
+    cur.execute(query_2)
+    country_ids = []
+    counts = []
 
-# Task 3
-ax[2].scatter(types, avg_durations, color='g')
-ax[2].set_title('Середня тривалість фільмів/шоу за типом')
-ax[2].set_xlabel('Тип')
-ax[2].set_ylabel('Середня тривалість')
+    for row in cur:
+        country_ids.append(row[0])
+        counts.append(row[1])
+
+    ax[1].pie(counts, labels=country_ids, autopct='%1.1f%%', 
+              wedgeprops={'edgecolor':'y'})
+    ax[1].set_title('Top 5 Countries by Rating Count')
+
+    # Task 3
+    cur.execute(query_3)
+    release_years = []
+    average_ranks = []
+
+    for row in cur:
+        release_years.append(row[0])
+        average_ranks.append(row[1])
+
+    ax[2].plot(release_years, average_ranks, color='g', marker='o')
+    ax[2].set_title('Average Rank by Release Year')
+    ax[2].set_xlabel('Release Year')
+    ax[2].set_ylabel('Average Rank')
+
+mng = plt.get_current_fig_manager()
+mng.resize(1400, 600)
 
 plt.show()
